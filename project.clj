09e7ba162f4eb27d416619bf1fc3295762bf0883
Nils Grunwald <nils@grunwald.fr>
2011-08-16 14:43:00 +0200

    added yaml dep to project

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject ring-middleware-format "0.1.0"
   :description "Ring middleware for parsing parameters and emitting responses in various formats"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                [clj-json "0.3.1"]])
+                 [clj-json "0.3.1"]
+                 [gfrlog/clj-yaml "0.3.0"]])
