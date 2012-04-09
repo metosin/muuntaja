cad68d8f674020965d902204148377392b5e69bd
Jani Rahkola <jani.rahkola@gmail.com>
2012-04-10 01:20:38 +0300

    Wrap-format-response obeys Accept headers.
    Wrap-format-response takes a sequence of encoders and selects the
    preferred one according to the Accept header. If no suitable encoder
    is found, 406 Not Acceptable is returned. Content-Type of the encoded
    string is given with the encoder, so wrap-format-response no longer
    takes a type parameter.
    
    Make-encoder takes an encoder function and Content-Type of the encoded
    string. Returns a map suitable for wrap-format-response.
    
    Wrap-<type>-response function uses the new wrap-format-response but
    the signature is unchanged.
    
    Wrap-restful-response uses the preferred encoder according to Accept,
    or returns 406 Not Acceptable. :default should be a map returned by
    make-encoder.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject ring-middleware-format "0.1.2-SNAPSHOT"
-  :description "Ring middleware for parsing parameters and emitting responses in various formats. See https://github.com/ngrunwald/ring-middleware-format"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [ring/ring-core "[0.3.11,]"]
+(defproject ring-middleware-format "0.2.0-SNAPSHOT"
+  :description "Ring middleware for parsing parameters and emitting
+  responses in various formats. See
+  https://github.com/ngrunwald/ring-middleware-format"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-core "[0.3.11,)"]
                  [cheshire "2.0.4"]
                  [gfrlog/clj-yaml "0.3.0"]])
