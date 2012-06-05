b829eeebb2c7a8d0a793a6e5fa9dee3c98c55bed
Nils Grunwald <nils@grunwald.fr>
2012-06-05 15:09:31 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject ring-middleware-format "0.2.0-SNAPSHOT"
+(defproject ring-middleware-format "0.2.0"
   :description "Ring middleware for parsing parameters and emitting
   responses in various formats. See
   https://github.com/ngrunwald/ring-middleware-format"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :url "https://github.com/ngrunwald/ring-middleware-format"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.memoize "0.5.1"]
                  [ring/ring-core "1.0.2"]
                  [cheshire "4.0.0"]
