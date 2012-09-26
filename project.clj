594bde7642aefaeb9a449b26236f54323d006ffc
Nils Grunwald <nils@grunwald.fr>
2012-09-27 01:24:50 +0200

    bump version and update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-middleware-format "0.3.0-SNAPSHOT"
+(defproject ring-middleware-format "0.2.1"
   :description "Ring middleware for parsing parameters and emitting
   responses in various formats."
   :url "https://github.com/ngrunwald/ring-middleware-format"
@@ -8,7 +8,7 @@
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.memoize "0.5.2"]
-                 [ring "1.1.5"]
-                 [cheshire "4.0.2"]
+                 [ring "1.1.6"]
+                 [cheshire "4.0.3"]
                  [com.ibm.icu/icu4j "4.8.1.1"]
                  [clj-yaml "0.4.0"]])
