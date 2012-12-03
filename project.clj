61f74399013ff85242b0a737b42eaae854d9ef17
Nils Grunwald <nils@grunwald.fr>
2012-12-03 19:48:54 +0100

    up version before release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-middleware-format "0.2.3-SNAPSHOT"
+(defproject ring-middleware-format "0.2.3"
   :description "Ring middleware for parsing parameters and emitting
   responses in various formats."
   :url "https://github.com/ngrunwald/ring-middleware-format"
