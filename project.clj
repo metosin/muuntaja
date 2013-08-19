3b14cd6eef861c70778041744c35304a0fd9098b
Nils Grunwald <nils@grunwald.fr>
2013-08-19 20:18:07 +0200

    updated all dependencies

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/core.memoize "0.5.2"]
-                 [ring "1.1.8"]
-                 [cheshire "5.0.1"]
-                 [org.clojure/tools.reader "0.7.0"]
-                 [com.ibm.icu/icu4j "50.1.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.memoize "0.5.6"]
+                 [ring "1.2.0"]
+                 [cheshire "5.2.0"]
+                 [org.clojure/tools.reader "0.7.6"]
+                 [com.ibm.icu/icu4j "51.2"]
                  [clj-yaml "0.4.0"]])
