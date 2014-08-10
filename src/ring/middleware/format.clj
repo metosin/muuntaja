d6a6c7abb579d2b8aaca8d66d9a5427ea31f7c94
Nils Grunwald <nils@grunwald.fr>
2014-08-10 18:25:53 +0200

    easier customizing of error handling in format

--- src/ring/middleware/format.clj
+++ src/ring/middleware/format.clj
@@ -11,11 +11,17 @@
    *[:json :edn :yaml :yaml-in-html :transit-msgpack :transit-json]*
    by default. There is also a *:json-kw* and *:yaml-kw* formats which
    use keywords as keys when deserializing. The first format is also
-   the default serialization format (*:json* by default)."
-  [handler & {:keys [formats]
+   the default serialization format (*:json* by default). You can also
+   specify error-handlers for the params parsing with *:request-error-handler*
+   or the response encoding with *:response-error-handler*. See
+   [[ring.middleware.format-params/wrap-format-params]] and
+   [[ring.middleware.format-response/wrap-format-response]] for details"
+  [handler & {:keys [formats response-error-handler request-error-handler]
               :or {formats [:json :edn :yaml :yaml-in-html
-                            :transit-msgpack :transit-json]}
+                            :transit-msgpack :transit-json]
+                   response-error-handler res/default-handle-error
+                   request-error-handler par/default-handle-error}
               :as options}]
   (-> handler
-      (par/wrap-restful-params :formats formats)
-      (res/wrap-restful-response :formats formats)))
+      (par/wrap-restful-params :formats formats :handle-error request-error-handler)
+      (res/wrap-restful-response :formats formats :handle-error response-error-handler)))
