b50f961905fb6d63c2783e690377ebbb651c0497
Juho Teperi <juho.teperi@iki.fi>
2015-09-01 18:38:36 +0300

    Add magical documentation script

--- push-doc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd doc
-git checkout gh-pages
-git add .
-git commit -am $1
-git push -u origin gh-pages
-cd ..
---
+++ scripts/build-docs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rev=$(git rev-parse HEAD)
+remoteurl=$(git ls-remote --get-url origin)
+
+git fetch
+if [[ -z $(git branch -r --list origin/gh-pages) ]]; then
+    # If repo doesn't have gh-pages branch, create it
+    (
+    mkdir doc
+    cd doc
+    git init
+    git remote add origin ${remoteurl}
+    git checkout -b gh-pages
+    git commit --allow-empty -m "Init"
+    git push -u origin gh-pages
+    )
+elif [[ ! -d doc ]]; then
+    # Clone existing gh-pages branch if not cloned
+    git clone --branch gh-pages ${remoteurl} doc
+else
+    # Reset existing clone to remote state
+    (
+    cd doc
+    git fetch
+    git reset --hard origin/gh-pages
+    )
+fi
+
+mkdir -p doc
+lein doc
+cd doc
+git add --all
+git commit -m "Build docs from ${rev}."
+git push origin gh-pages
