5c99a23dca4b98cfca4b18784153c0538321f3d2
Tommi Reiman <tommi@iki.fi>
2017-01-03 23:35:56 +0200

    Reorganize muuntaja.core

--- src/muuntaja/core.clj
+++ src/muuntaja/core.clj
@@ -24,115 +24,6 @@
 (defn- assoc-assoc [m k1 k2 v]
   (assoc m k1 (assoc (k1 m) k2 v)))
 
-(defn- fail-on-request-decode-exception [m
-                                         ^Exception e
-                                         ^FormatAndCharset request-format-and-charset
-                                         ^FormatAndCharset response-format-and-charset
-                                         request]
-  (throw
-    (ex-info
-      (str "Malformed " (:format request-format-and-charset) " request.")
-      {:type ::decode
-       :default-format (:default-format m)
-       :format (:format request-format-and-charset)
-       :charset (:charset request-format-and-charset)
-       :request request}
-      e)))
-
-(defn- fail-on-request-charset-negotiation [formats]
-  (throw
-    (ex-info
-      "Can't negotiate on request charset"
-      {:type ::request-charset-negotiation
-       :charsets (:charsets formats)})))
-
-(defn- fail-on-response-charset-negotiation [formats]
-  (throw
-    (ex-info
-      "Can't negotiate on response charset"
-      {:type ::response-charset-negotiation
-       :charsets (:charsets formats)})))
-
-(defn- fail-on-response-format-negotiation [formats]
-  (throw
-    (ex-info
-      "Can't negotiate on response format"
-      {:type ::response-format-negotiation
-       :formats (:produces formats)})))
-
-(defn- set-content-type [response content-type]
-  (assoc-assoc response :headers "Content-Type" content-type))
-
-(defn- content-type [format charset]
-  (str format "; charset=" charset))
-
-;;
-;; Memoizable negotiation functions
-;;
-
-(defn- -negotiate-content-type [{:keys [consumes matchers default-charset charsets] :as formats} s]
-  (if s
-    (let [[content-type-raw charset-raw] (parse/parse-content-type s)]
-      (records/->FormatAndCharset
-        (if content-type-raw
-          (or (consumes content-type-raw)
-              (some
-                (fn [[name r]]
-                  (if (and r (re-find r content-type-raw)) name))
-                matchers)))
-        (or
-          ;; if a provided charset was valid
-          (and charset-raw charsets (charsets charset-raw) charset-raw)
-          ;; only set default if none were set
-          (and (not charset-raw) default-charset)
-          ;; negotiation failed
-          (fail-on-request-charset-negotiation formats))))))
-
-;; TODO: fail if no match?
-(defn- -negotiate-accept [{:keys [produces default-format]} s]
-  (or
-    (some-value
-      produces
-      (parse/parse-accept s))
-    default-format))
-
-;; TODO: fail if no match?
-(defn- -negotiate-accept-charset [{:keys [default-charset charsets]} s]
-  (or
-    (some-value
-      (or charsets identity)
-      (parse/parse-accept-charset s))
-    default-charset))
-
-;;
-;; Content Negotiation
-;;
-
-(defn negotiate-request [^Muuntaja m request]
-  ((.negotiate_content_type m)
-    ((.extract_content_type m)
-      request)))
-
-(defn negotiate-response [^Muuntaja m request]
-  (records/->FormatAndCharset
-    ((.negotiate_accept m)
-      ((.extract_accept m)
-        request))
-    ((.negotiate_accept_charset m)
-      ((.extract_accept_charset m)
-        request))))
-
-(defn decode-request? [^Muuntaja m request]
-  (if-let [decode? (.decode_request_body_QMARK_ m)]
-    (and (not (contains? request ::format))
-         (decode? request))))
-
-(defn encode-response? [^Muuntaja m request response]
-  (if-let [encode? (.encode_response_body_QMARK_ m)]
-    (and (map? response)
-         (not (contains? response ::format))
-         (encode? request response))))
-
 ;;
 ;; encode & decode
 ;;
@@ -240,42 +131,29 @@
     options))
 
 (declare default-options)
+(declare memoized-http-negotiation)
 
 (defn- -create
   [{:keys [formats default-format default-charset http] :as options}]
   (let [adapters (create-adapters formats default-charset)
-        valid-format? (key-set formats identity)
-        m (records/map->Muuntaja
-            (merge
-              (dissoc options :formats :http)
-              ;; TODO: don't copy here.
-              (http-options http)
-              {:adapters adapters
-               :consumes (key-set formats :decoder)
-               :produces (key-set formats :encoder)
-               :matchers (matchers formats)}))]
+        valid-format? (key-set formats identity)]
     (when-not (or (not default-format) (valid-format? default-format))
       (throw
         (ex-info
           (str "Invalid default format " default-format)
           {:formats valid-format?
            :default-format default-format})))
-    (-> m
-        (assoc
-          :negotiate-accept-charset
-          (parse/fast-memoize
-            (parse/cache 1000)
-            (partial -negotiate-accept-charset m)))
-        (assoc
-          :negotiate-accept
-          (parse/fast-memoize
-            (parse/cache 1000)
-            (partial -negotiate-accept m)))
-        (assoc
-          :negotiate-content-type
-          (parse/fast-memoize
-            (parse/cache 1000)
-            (partial -negotiate-content-type m))))))
+    (->
+      (merge
+        (dissoc options :formats :http)
+        ;; TODO: don't copy here.
+        (http-options http)
+        {:adapters adapters
+         :consumes (key-set formats :decoder)
+         :produces (key-set formats :encoder)
+         :matchers (matchers formats)})
+      (records/map->Muuntaja)
+      (memoized-http-negotiation))))
 
 (defn create
   "Creates a new Muuntaja from a given prototype:
@@ -288,6 +166,216 @@
    (if (instance? Muuntaja prototype)
      prototype
      (-create prototype))))
+
+;;
+;; transforming options
+;;
+
+(defn transform-formats [options f]
+  (update options :formats #(into (empty %) (map (fn [[k v]] [k (f k v)]) %))))
+
+(defn select-formats [options formats]
+  (let [existing-formats (-> options :formats keys set)
+        future-formats (set formats)]
+    (when-let [diff (seq (set/difference future-formats existing-formats))]
+      (throw
+        (ex-info
+          (str "invalid formats: " diff)
+          {:invalid (seq diff)
+           :formats (seq formats)
+           :existing (seq existing-formats)})))
+    (-> options
+        (update :formats select-keys formats)
+        (assoc :default-format (first formats)))))
+
+;;
+;; default options
+;;
+
+(defn extract-content-type-ring
+  "Extracts content-type from ring-request."
+  [request]
+  (get (:headers request) "content-type"))
+
+(defn extract-accept-ring
+  "Extracts accept from ring-request."
+  [request]
+  (get (:headers request) "accept"))
+
+(defn extract-accept-charset-ring
+  "Extracts accept-charset from ring-request."
+  [request]
+  (get (:headers request) "accept-charset"))
+
+(defn encode-collections-with-override [_ response]
+  (or
+    (-> response ::encode?)
+    (-> response :body coll?)))
+
+(def available-charsets
+  "Set of recognised charsets by the current JVM"
+  (into #{} (map str/lower-case (.keySet (Charset/availableCharsets)))))
+
+(def default-options
+  {:http {:extract-content-type extract-content-type-ring
+          :extract-accept-charset extract-accept-charset-ring
+          :extract-accept extract-accept-ring
+          :decode-request-body? (constantly true)
+          :encode-response-body? encode-collections-with-override}
+
+   :default-charset "utf-8"
+   :charsets available-charsets
+
+   :default-format "application/json"
+   :formats {"application/json" {:decoder [formats/make-json-decoder {:key-fn true}]
+                                 :encoder [formats/make-json-encoder]
+                                 :encode-protocol [formats/EncodeJson formats/encode-json]}
+             "application/edn" {:decoder [formats/make-edn-decoder]
+                                :encoder [formats/make-edn-encoder]
+                                :encode-protocol [formats/EncodeEdn formats/encode-edn]}
+             "application/msgpack" {:decoder [formats/make-msgpack-decoder {:keywords? true}]
+                                    :encoder [formats/make-msgpack-encoder]
+                                    :encode-protocol [formats/EncodeMsgpack formats/encode-msgpack]}
+             "application/x-yaml" {:decoder [formats/make-yaml-decoder {:keywords true}]
+                                   :encoder [formats/make-yaml-encoder]
+                                   :encode-protocol [formats/EncodeYaml formats/encode-yaml]}
+             "application/transit+json" {:decoder [(partial formats/make-transit-decoder :json)]
+                                         :encoder [(partial formats/make-transit-encoder :json)]
+                                         :encode-protocol [formats/EncodeTransitJson formats/encode-transit-json]}
+             "application/transit+msgpack" {:decoder [(partial formats/make-transit-decoder :msgpack)]
+                                            :encoder [(partial formats/make-transit-encoder :msgpack)]
+                                            :encode-protocol [formats/EncodeTransitMessagePack formats/encode-transit-msgpack]}}})
+
+;;
+;; HTTP stuff
+;;
+
+(defn- fail-on-request-decode-exception [m
+                                         ^Exception e
+                                         ^FormatAndCharset request-format-and-charset
+                                         ^FormatAndCharset response-format-and-charset
+                                         request]
+  (throw
+    (ex-info
+      (str "Malformed " (:format request-format-and-charset) " request.")
+      {:type ::decode
+       :default-format (:default-format m)
+       :format (:format request-format-and-charset)
+       :charset (:charset request-format-and-charset)
+       :request request}
+      e)))
+
+(defn- fail-on-request-charset-negotiation [formats]
+  (throw
+    (ex-info
+      "Can't negotiate on request charset"
+      {:type ::request-charset-negotiation
+       :charsets (:charsets formats)})))
+
+(defn- fail-on-response-charset-negotiation [formats]
+  (throw
+    (ex-info
+      "Can't negotiate on response charset"
+      {:type ::response-charset-negotiation
+       :charsets (:charsets formats)})))
+
+(defn- fail-on-response-format-negotiation [formats]
+  (throw
+    (ex-info
+      "Can't negotiate on response format"
+      {:type ::response-format-negotiation
+       :formats (:produces formats)})))
+
+(defn- set-content-type [response content-type]
+  (assoc-assoc response :headers "Content-Type" content-type))
+
+(defn- content-type [format charset]
+  (str format "; charset=" charset))
+
+;;
+;; Memoizable negotiation functions
+;;
+
+(defn- -negotiate-content-type [{:keys [consumes matchers default-charset charsets] :as formats} s]
+  (if s
+    (let [[content-type-raw charset-raw] (parse/parse-content-type s)]
+      (records/->FormatAndCharset
+        (if content-type-raw
+          (or (consumes content-type-raw)
+              (some
+                (fn [[name r]]
+                  (if (and r (re-find r content-type-raw)) name))
+                matchers)))
+        (or
+          ;; if a provided charset was valid
+          (and charset-raw charsets (charsets charset-raw) charset-raw)
+          ;; only set default if none were set
+          (and (not charset-raw) default-charset)
+          ;; negotiation failed
+          (fail-on-request-charset-negotiation formats))))))
+
+;; TODO: fail if no match?
+(defn- -negotiate-accept [{:keys [produces default-format]} s]
+  (or
+    (some-value
+      produces
+      (parse/parse-accept s))
+    default-format))
+
+;; TODO: fail if no match?
+(defn- -negotiate-accept-charset [{:keys [default-charset charsets]} s]
+  (or
+    (some-value
+      (or charsets identity)
+      (parse/parse-accept-charset s))
+    default-charset))
+
+(defn memoized-http-negotiation [^Muuntaja m]
+  (-> m
+      (assoc
+        :negotiate-accept-charset
+        (parse/fast-memoize
+          (parse/cache 1000)
+          (partial -negotiate-accept-charset m)))
+      (assoc
+        :negotiate-accept
+        (parse/fast-memoize
+          (parse/cache 1000)
+          (partial -negotiate-accept m)))
+      (assoc
+        :negotiate-content-type
+        (parse/fast-memoize
+          (parse/cache 1000)
+          (partial -negotiate-content-type m)))))
+
+;;
+;; Content Negotiation
+;;
+
+(defn negotiate-request [^Muuntaja m request]
+  ((.negotiate_content_type m)
+    ((.extract_content_type m)
+      request)))
+
+(defn negotiate-response [^Muuntaja m request]
+  (records/->FormatAndCharset
+    ((.negotiate_accept m)
+      ((.extract_accept m)
+        request))
+    ((.negotiate_accept_charset m)
+      ((.extract_accept_charset m)
+        request))))
+
+(defn decode-request? [^Muuntaja m request]
+  (if-let [decode? (.decode_request_body_QMARK_ m)]
+    (and (not (contains? request ::format))
+         (decode? request))))
+
+(defn encode-response? [^Muuntaja m request response]
+  (if-let [encode? (.encode_response_body_QMARK_ m)]
+    (and (map? response)
+         (not (contains? response ::format))
+         (encode? request response))))
 
 ;;
 ;; Request
@@ -354,85 +442,6 @@
           (if-let [encoder (encoder m format)]
             (handle-response response format encoder charset)))))
     response))
-
-;;
-;; Options
-;;
-
-(defn extract-content-type-ring
-  "Extracts content-type from ring-request."
-  [request]
-  (get (:headers request) "content-type"))
-
-(defn extract-accept-ring
-  "Extracts accept from ring-request."
-  [request]
-  (get (:headers request) "accept"))
-
-(defn extract-accept-charset-ring
-  "Extracts accept-charset from ring-request."
-  [request]
-  (get (:headers request) "accept-charset"))
-
-(defn encode-collections-with-override [_ response]
-  (or
-    (-> response ::encode?)
-    (-> response :body coll?)))
-
-(def available-charsets
-  "Set of recognised charsets by the current JVM"
-  (into #{} (map str/lower-case (.keySet (Charset/availableCharsets)))))
-
-(def default-options
-  {:http {:extract-content-type extract-content-type-ring
-          :extract-accept-charset extract-accept-charset-ring
-          :extract-accept extract-accept-ring
-          :decode-request-body? (constantly true)
-          :encode-response-body? encode-collections-with-override}
-
-   :default-charset "utf-8"
-   :charsets available-charsets
-
-   :default-format "application/json"
-   :formats {"application/json" {:decoder [formats/make-json-decoder {:key-fn true}]
-                                 :encoder [formats/make-json-encoder]
-                                 :encode-protocol [formats/EncodeJson formats/encode-json]}
-             "application/edn" {:decoder [formats/make-edn-decoder]
-                                :encoder [formats/make-edn-encoder]
-                                :encode-protocol [formats/EncodeEdn formats/encode-edn]}
-             "application/msgpack" {:decoder [formats/make-msgpack-decoder {:keywords? true}]
-                                    :encoder [formats/make-msgpack-encoder]
-                                    :encode-protocol [formats/EncodeMsgpack formats/encode-msgpack]}
-             "application/x-yaml" {:decoder [formats/make-yaml-decoder {:keywords true}]
-                                   :encoder [formats/make-yaml-encoder]
-                                   :encode-protocol [formats/EncodeYaml formats/encode-yaml]}
-             "application/transit+json" {:decoder [(partial formats/make-transit-decoder :json)]
-                                         :encoder [(partial formats/make-transit-encoder :json)]
-                                         :encode-protocol [formats/EncodeTransitJson formats/encode-transit-json]}
-             "application/transit+msgpack" {:decoder [(partial formats/make-transit-decoder :msgpack)]
-                                            :encoder [(partial formats/make-transit-encoder :msgpack)]
-                                            :encode-protocol [formats/EncodeTransitMessagePack formats/encode-transit-msgpack]}}})
-
-;;
-;; transforming options
-;;
-
-(defn transform-formats [options f]
-  (update options :formats #(into (empty %) (map (fn [[k v]] [k (f k v)]) %))))
-
-(defn select-formats [options formats]
-  (let [existing-formats (-> options :formats keys set)
-        future-formats (set formats)]
-    (when-let [diff (seq (set/difference future-formats existing-formats))]
-      (throw
-        (ex-info
-          (str "invalid formats: " diff)
-          {:invalid (seq diff)
-           :formats (seq formats)
-           :existing (seq existing-formats)})))
-    (-> options
-        (update :formats select-keys formats)
-        (assoc :default-format (first formats)))))
 
 ;;
 ;; request helpers
