97cd9f23d7f19e94dc7d4191da061db35f98e0ad
Tommi Reiman <tommi@iki.fi>
2017-01-04 23:03:49 +0200

    Move namespaces

--- src/clj/muuntaja/format/json.clj
+++ src/clj/muuntaja/format/json.clj
@@ -1,35 +1,36 @@
 (ns muuntaja.format.json
-  (:require [cheshire.core :as json]
+  (:require [cheshire.core :as cheshire]
             [cheshire.parse :as parse]
             [muuntaja.protocols :as protocols]
-            [msgpack.clojure-extensions])
+            [msgpack.clojure-extensions]
+            [muuntaja.json :as json])
   (:import (java.io InputStreamReader InputStream ByteArrayInputStream OutputStreamWriter OutputStream)))
 
 (defn make-json-decoder [{:keys [key-fn array-coerce-fn bigdecimals?]}]
   (if-not bigdecimals?
     (fn [x ^String charset]
       (if (string? x)
-        (json/parse-string x key-fn array-coerce-fn)
-        (json/parse-stream (InputStreamReader. ^InputStream x charset) key-fn array-coerce-fn)))
+        (cheshire/parse-string x key-fn array-coerce-fn)
+        (cheshire/parse-stream (InputStreamReader. ^InputStream x charset) key-fn array-coerce-fn)))
     (fn [x ^String charset]
       (binding [parse/*use-bigdecimals?* bigdecimals?]
         (if (string? x)
-          (json/parse-string x key-fn array-coerce-fn)
-          (json/parse-stream (InputStreamReader. ^InputStream x charset) key-fn array-coerce-fn))))))
+          (cheshire/parse-string x key-fn array-coerce-fn)
+          (cheshire/parse-stream (InputStreamReader. ^InputStream x charset) key-fn array-coerce-fn))))))
 
 (defn make-json-encoder [options]
   (fn [data ^String charset]
-    (ByteArrayInputStream. (.getBytes (json/generate-string data options) charset))))
+    (ByteArrayInputStream. (.getBytes (cheshire/generate-string data options) charset))))
 
 (defn make-json-string-encoder [options]
   (fn [data _]
-    (json/generate-string data options)))
+    (cheshire/generate-string data options)))
 
 (defn make-streaming-json-encoder [options]
   (fn [data ^String charset]
     (protocols/->StreamableResponse
       (fn [^OutputStream output-stream]
-        (json/generate-stream
+        (cheshire/generate-stream
           data
           (OutputStreamWriter. output-stream charset)
           options)
@@ -37,6 +38,17 @@
 
 (defprotocol EncodeJson
   (encode-json [this charset]))
+
+;; muuntaja.json - experimental
+
+(defn ^:no-doc make-muuntaja-json-decoder [{:keys [key-fn array-coerce-fn bigdecimals?] :as options}]
+  (assert (not (seq options)) (str "Muuntaja JSON doesn't yet allow options:" options))
+  (fn [x ^String charset]
+    (json/from-json x)))
+
+(defn ^:no-doc make-muuntaja-json-encoder [options]
+  (fn [data ^String charset]
+    (ByteArrayInputStream. (.getBytes (json/to-json data) charset))))
 
 ;;
 ;; format
