6b53ca39d8c8fba8b0c04c1ce086e8f03afc36c1
Tommi Reiman <tommi@iki.fi>
2017-02-12 17:17:36 +0200

    Cleanup interceptors

--- src/clj/muuntaja/interceptor.clj
+++ src/clj/muuntaja/interceptor.clj
@@ -7,7 +7,5 @@
   (let [formats (m/create prototype)]
     (map->Interceptor
       {:name ::format
-       :enter (fn [ctx]
-                (update ctx :request (partial m/format-request formats)))
-       :leave (fn [ctx]
-                (update ctx :response (partial m/format-response formats (:request ctx))))})))
+       :enter #(update % :request (partial m/format-request formats))
+       :leave #(update % :response (partial m/format-response formats (:request %)))})))
