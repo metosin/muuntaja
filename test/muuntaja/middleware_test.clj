cdd86f27961f85db940ded2cd952959b1fad13eb
Daniel Mason <dan@juxt.pro>
2019-08-23 15:03:30 +0100

    Apply middleware within the middleware wildcard tests

--- test/muuntaja/middleware_test.clj
+++ test/muuntaja/middleware_test.clj
@@ -176,11 +176,11 @@
 
     (testing "wildcards (name/*) & (*/*) successfully negotiate"
       (let [app (middleware/wrap-format echo (dissoc m/default-options :default-format))
-            response  (->request "application/json" "application/*" nil json-string)
-            response' (->request "application/json" "*/*" nil json-string)]
+            response  (app  (->request "application/json" "application/*" nil json-string))
+            response' (app (->request "application/json" "*/*" nil json-string))]
 
-        (is (= json-string (:body response)))
-        (is (= json-string (:body response')))))
+        (is (= json-string (slurp (:body response))))
+        (is (= json-string (slurp (:body response'))))))
 
     (testing "without :default-format & valid accept format, response format negotiation fails"
       (let [app (middleware/wrap-format echo (dissoc m/default-options :default-format))]
