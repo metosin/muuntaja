0e4c14d570fc66b17942768d934308337c90ddf0
Miikka Koskinen <miikka@metosin.fi>
2019-11-01 21:54:33 +0200

    Add :scm information to project.clj files

--- modules/muuntaja-msgpack/project.clj
+++ modules/muuntaja-msgpack/project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/metosin/muuntaja"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/metosin/muuntaja"
+        :dir "../.."}
   :plugins [[lein-parent "0.3.2"]]
   :parent-project {:path "../../project.clj"
                    :inherit [:deploy-repositories :managed-dependencies]}
