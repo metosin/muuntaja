347412cbf5b7d162f97457f6b6a377c3b6203c35
Joel Kaasinen <joel.kaasinen@metosin.fi>
2025-01-09 11:48:28 +0200

    deps: bump
    ignoring new pedestal 0.7.2 for now because it breaks

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :managed-dependencies [[metosin/muuntaja "0.6.10"]
-                         [ring/ring-codec "1.1.2"]
-                         [metosin/jsonista "0.3.1"]
-                         [com.cognitect/transit-clj "1.0.324"]
-                         [com.cnuernber/charred "1.033"]
-                         [cheshire "5.10.0"]
-                         [clj-commons/clj-yaml "1.0.27"]
+                         [ring/ring-codec "1.2.0"]
+                         [metosin/jsonista "0.3.13"]
+                         [com.cognitect/transit-clj "1.0.333"]
+                         [com.cnuernber/charred "1.034"]
+                         [cheshire "5.13.0"]
+                         [clj-commons/clj-yaml "1.0.29"]
                          [clojure-msgpack "1.2.1" :exclusions [org.clojure/clojure]]]
   :deploy-repositories [["releases" {:url "https://repo.clojars.org/"
                                      :sign-releases false
@@ -35,11 +35,11 @@
                                   "modules/muuntaja-msgpack/src"]
 
                    :dependencies [[org.clojure/clojure "1.12.0"]
-                                  [com.cnuernber/charred "1.033"]
-                                  [ring/ring-core "1.9.0"]
-                                  [ring-middleware-format "0.7.4"]
+                                  [com.cnuernber/charred "1.034"]
+                                  [ring/ring-core "1.13.0"]
+                                  [ring-middleware-format "0.7.5"]
                                   [ring-transit "0.1.6"]
-                                  [ring/ring-json "0.5.0"]
+                                  [ring/ring-json "0.5.1"]
 
                                   ;; modules
                                   [metosin/muuntaja "0.6.10"]
@@ -50,18 +50,18 @@
                                   [metosin/muuntaja-yaml "0.6.10"]
 
                                   ;; correct jackson
-                                  [com.fasterxml.jackson.core/jackson-databind "2.12.1"]
+                                  [com.fasterxml.jackson.core/jackson-databind "2.18.2"]
 
                                   ;; Sieppari
                                   [metosin/sieppari "0.0.0-alpha5"]
 
                                   ;; Pedestal
-                                  [org.clojure/core.async "1.3.610" :exclusions [org.clojure/tools.reader]]
+                                  [org.clojure/core.async "1.7.701" :exclusions [org.clojure/tools.reader]]
                                   [io.pedestal/pedestal.service "0.5.8" :exclusions [org.clojure/tools.reader
                                                                                      org.clojure/core.async
                                                                                      org.clojure/core.memoize]]
                                   [javax.servlet/javax.servlet-api "4.0.1"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.30"]
+                                  [org.slf4j/slf4j-log4j12 "2.0.16"]
 
                                   [criterium "0.4.6"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
